f6d6dee84e56ab254c28f1ff012476d4574c3c90
ShawnKu <ShawnKu@users.noreply.github.com>
2014-09-30 18:01:21 +1000

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,3 +1,3 @@
 <?php
-mysql_select_db('filerepository',mysql_connect('localhost','root',''))or die(mysql_error());
+mysql_select_db('filerepository',mysql_connect('localhost','webadmin','123'))or die(mysql_error());
 ?>
