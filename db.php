c76e0a3a1bf96a6789697bef2afe4cbdea6c7b72
ShawnKu <ShawnKu@users.noreply.github.com>
2014-09-30 18:00:53 +1000

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'filerepository');
-define('DB_USER', 'root');
-define('DB_PW', '');
+define('DB_USER', 'webadmin');
+define('DB_PW', '123');
 ?>
 
